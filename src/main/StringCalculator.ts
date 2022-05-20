1e2b7757cf61a5811dd8057528ad4cdeae8532fd
Giles <steven.giles@steven.giles.macbook>
2022-05-20 11:10:38 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/StringCalculator.ts

--- src/main/StringCalculator.ts
+++ src/main/StringCalculator.ts
@@ -1,7 +1,32 @@
 export class StringCalculator {
     
-    add(number: string) {
-        
+    add(numbers: string) {
+        if(numbers !== "") {
+
+            let delimiter = ",";
+            let t = numbers;
+            if (numbers.startsWith("//")) {
+                delimiter = numbers.charAt(2);
+                t = numbers.substring(4);
+            }
+
+            let t1 = t.replace(/\n/g,',');
+            const regex = "/[,]{1,}";
+
+            if (t1.match(regex)) {
+                throw new Error("Error");
+            }
+
+            const numberArray = t1.split(delimiter);
+            if(numberArray.length === 1) {
+                return +numbers;
+            }
+            let sum = 0;
+            numberArray.forEach(number => {
+                sum += +number;
+            });
+            return sum;
+        }
         return 0;
         
     }
--- src/test/StringCalculator.test.ts
+++ src/test/StringCalculator.test.ts
@@ -1,8 +1,89 @@
 import {StringCalculator} from "../main/StringCalculator";
 
 describe('string calculator', () => {
-    it("should give true for year 1996", () => {
+    it("should return 0 if empty string", () => {
         let stringCalculator: StringCalculator = new StringCalculator();
         expect(stringCalculator.add("")).toBe(0);
+    })        
+
+    describe('should return the given number if we have only one number', () => {
+        it.each`
+        input        | expected
+        ${"1"}       | ${1}
+        ${"2"}       | ${2}
+        ${"3"}       | ${3}
+        `('should print $expected if the input is "$input"', ({ input, expected }) => {
+            let stringCalculator: StringCalculator = new StringCalculator();
+            expect(stringCalculator.add(input)).toBe(expected);
+
+        });
+    })
+
+    describe('should return the sum if we have two numbers', () => {
+        it.each`
+        input        | expected
+        ${"1,1"}     | ${2}
+        ${"2,2"}     | ${4}
+        ${"4,2"}     | ${6}
+        `('should print $expected if the input is "$input"', ({ input, expected }) => {
+            let stringCalculator: StringCalculator = new StringCalculator();
+            expect(stringCalculator.add(input)).toBe(expected);
+
+        });
+    })
+
+    describe('should return the sum if an unknown amount of numbers', () => {
+        it.each`
+        input        | expected
+        ${"1,1"}     | ${2}
+        ${"1,1,1"}   | ${3}
+        ${"1,1,1,1,1"}   | ${5}
+        `('should print $expected if the input is "$input"', ({ input, expected }) => {
+            let stringCalculator: StringCalculator = new StringCalculator();
+            expect(stringCalculator.add(input)).toBe(expected);
+
+        });
+    })
+
+    describe('should return the sum even with new lines between numbers', () => {
+        it.each`
+        input        | expected
+        ${"1\n1"}    | ${2}
+        ${"2\n2\n2"} | ${6}
+        ${"2\n2,2"}  | ${6}
+        `('should print $expected if the input is "$input"', ({ input, expected }) => {
+            let stringCalculator: StringCalculator = new StringCalculator();
+            expect(stringCalculator.add(input)).toBe(expected);
+        });
+
+        it("should return an error if string ends in \\n", () => {
+            let stringCalculator: StringCalculator = new StringCalculator();
+            try {
+                stringCalculator.add("1,\n");
+            } catch (error) {
+                expect(error).toHaveBeenCalled;
+            }
+        }) 
+
+        it("should return an error if string ends in multiple \\n", () => {
+            let stringCalculator: StringCalculator = new StringCalculator();
+            try {
+                stringCalculator.add("1,\n\n");
+            } catch (error) {
+                expect(error).toHaveBeenCalled;
+            }
+        }) 
+    })
+
+
+    describe('should return the sum using different delimiters', () => {
+        it.each`
+        input           | expected
+        ${"//,\n1,1"}   | ${2}
+        ${"//;\n1;1"}   | ${2}
+        `('should print $expected if the input is "$input"', ({ input, expected }) => {
+            let stringCalculator: StringCalculator = new StringCalculator();
+            expect(stringCalculator.add(input)).toBe(expected);
+        });
     })
 })
