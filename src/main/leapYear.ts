63bd4abdf09ba4dfd411974ddca3d164e07e7993
Giles <steven.giles@steven.giles.macbook>
2022-05-20 09:44:31 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/leapYear.ts

--- src/main/leapYear.ts
+++ src/main/leapYear.ts
@@ -1,6 +1,10 @@
 export class LeapYear {
     
-    calc(year: number) {
-        return true;
+    isLeapYear(year: number) {
+        if((year % 4 !== 0) || ((year % 100 === 0 && year % 400 !== 0))) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
